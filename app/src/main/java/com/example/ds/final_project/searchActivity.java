01d924b877f178cab21acaf68754a39346164319
wltjs188 <wltjs188@gamil.com>
2019-03-18 15:15:27 +0900

    챗봇 적용

--- app/src/main/java/com/example/ds/final_project/searchActivity.java
+++ app/src/main/java/com/example/ds/final_project/searchActivity.java
@@ -1,9 +1,14 @@
 package com.example.ds.final_project;
 
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,18 +19,35 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.JsonElement;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import ai.api.AIListener;
+import ai.api.android.AIConfiguration;
+import ai.api.android.AIService;
+import ai.api.model.AIError;
+import ai.api.model.AIResponse;
+import ai.api.model.Result;
 
 
-public class searchActivity extends AppCompatActivity {
+public class searchActivity extends AppCompatActivity implements AIListener {
     //상품검색
+
+    AIService aiService;
+    TextView t;
+
+
     private ListView listView;
     private View btnSend;
     private EditText editText;
     boolean isMine;
     private List<ChatMessage> chatMessages;
     private ArrayAdapter<ChatMessage> adapter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +66,19 @@ public class searchActivity extends AppCompatActivity {
         listView.setAdapter(adapter);
         //ChatMessage chatMessage = new ChatMessage("어떤 상품을 찾아 드릴까요?", true);
         //chatMessages.add(chatMessage);
+        setContentView(R.layout.activity_main);
+        t=(TextView)findViewById(R.id.textView);
+        int permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            makeRequest();
+        }
+        final AIConfiguration config = new AIConfiguration("7e5e4005e6cf487385649f1c6f88d37f",
+                AIConfiguration.SupportedLanguages.Korean,
+                AIConfiguration.RecognitionEngine.System);
+        aiService = AIService.getService(this, config);
+        aiService.setListener(this);
 
 
         btnSend.setOnClickListener(new View.OnClickListener() {
@@ -62,6 +97,67 @@ public class searchActivity extends AppCompatActivity {
             }
         });
 
+    }
+    protected void makeRequest() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.RECORD_AUDIO},
+                101);
+    }
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 101: {
+
+                if (grantResults.length == 0
+                        || grantResults[0] !=
+                        PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+
+                }
+                return;
+            }
+        }
+    }
+    public void ButtonClicked(View view){
+        aiService.startListening();
+    }
+
+    @Override
+    public void onResult(AIResponse result) { //챗봇에서 가져온 내용
+        Log.d("anu",result.toString());
+        Result result1=result.getResult();
+        String parameterString = "";
+        if (result1.getParameters() != null && !result1.getParameters().isEmpty()) {
+            for (final Map.Entry<String, JsonElement> entry : result1.getParameters().entrySet()) {
+                parameterString += "(" + entry.getKey() + ", " + entry.getValue();
+            }
+        }
+
+        t.setText("query:"+result1.getResolvedQuery()+"\naction:"+result1.getAction()+"\nanswer:"+ parameterString+"\n"+result1.getFulfillment().getSpeech());
+    }
+
+    @Override
+    public void onError(AIError error) {
+
+    }
+
+    @Override
+    public void onAudioLevel(float level) {
+
+    }
+
+    @Override
+    public void onListeningStarted() {
+    }
+
+    @Override
+    public void onListeningCanceled() {
+
+    }
+
+    @Override
+    public void onListeningFinished() {
     }
     public boolean onOptionsItemSelected(MenuItem item) { //뒤로가기버튼 실행
         switch (item.getItemId()){
