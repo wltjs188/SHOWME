810ec56d0ce6b2a739ed77b38062c3b4afb25f8f
wltjs188 <wltjs188@gamil.com>
2019-05-10 23:09:07 +0900

    insertWishList.php 수정

--- php/getWishList.php
+++ php/getWishList.php
@@ -1,33 +1,41 @@
 <?php 
-
     error_reporting(E_ALL); 
     ini_set('display_errors',1); 
-
     include('dbcon.php');
-        
-
-    $stmt = $con->prepare('select * from wishList');
-    $stmt->execute();
-
-    if ($stmt->rowCount() > 0)
+    $android = strpos($_SERVER['HTTP_USER_AGENT'], "Android");
+    if( (($_SERVER['REQUEST_METHOD'] == 'POST') && isset($_POST['submit'])) || $android )
     {
-        $data = array(); 
-
-        while($row=$stmt->fetch(PDO::FETCH_ASSOC))
+        // 안드로이드 코드의 postParameters 변수에 적어준 이름을 가지고 값을 전 
+        $uuid=$_POST['uuid'];
+        $productURL=$_POST['productURL'];
+        $info=$_POST['info'];
+        $image=$_POST['image'];
+        
+        if(!isset($errMSG)) // 이름과 나라 모두 입력이 되었다면 
         {
-            extract($row);
-    
-            array_push($data, 
-                array('uuid'=>$uuid,
-                'productURL'=>$productURL,
-                'info'=>$info
-                'image'=>$image
-            ));
+            
+                 try{
+                // SQL문을 실행하여 데이터를 MySQL 서버의 person 테이블에 저장합니다. 
+                $stmt = $con->prepare('INSERT INTO wishList(uuid,productURL,info, image) VALUES(:uuid, :productURL, :info, :image)');
+                $stmt->bindParam(':uuid', $uuid);
+                $stmt->bindParam(':productURL', $productURL);
+                $stmt->bindParam(':info', $info);
+                $stmt->bindParam(':image', $image);
+                
+                if($stmt->execute())
+                {
+                    $successMSG = "관심상품 등록되었습니다.";
+                }
+                else
+                {
+                    $errMSG = "사용자 추가 에러";
+                }
+            } catch(PDOException $e) {
+                die("Database error: " . $e->getMessage()); 
+            }
+            } catch(PDOException $e) {
+                die("Database error: " . $e->getMessage()); 
+            }
         }
-
-        header('Content-Type: application/json; charset=utf8');
-        $json = json_encode(array("wishList"=>$data), JSON_PRETTY_PRINT+JSON_UNESCAPED_UNICODE);
-        echo $json;
     }
-
 ?>
