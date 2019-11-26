38fba854bbac4ceb951e0dac54582310075179fa
chaeyoon <yoonchae125@naver.com>
2019-11-26 16:07:09 +0900

    ㅡ^ㅡ

---
+++ app/src/main/java/com/example/ds/final_project/db/DAO/ServertestActivity.java
@@ -0,0 +1,13 @@
+package com.example.ds.final_project.db.DAO;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ServertestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_servertest);
+    }
+}
