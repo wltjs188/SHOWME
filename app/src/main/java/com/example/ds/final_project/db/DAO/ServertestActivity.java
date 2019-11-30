cd0def84c40e6a8ea068710d567ee1a6eb48c04d
chaeyoon <yoonchae125@naver.com>
2019-11-30 13:51:39 +0900

    no message

--- app/src/main/java/com/example/ds/final_project/db/DAO/ServertestActivity.java
+++ app/src/main/java/com/example/ds/final_project/db/DAO/ServertestActivity.java
@@ -22,6 +22,7 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
@@ -47,12 +48,12 @@ public class ServertestActivity extends AppCompatActivity {
         setContentView(R.layout.activity_servertest);
 
         getList a = new getList();
-        a.execute();
+        a.execute("DeleteWishProduct","aaaa");
     }
 
 
 
-    class getList extends AsyncTask<Void, String, Void> {
+    class getList extends AsyncTask<String, Void,String> {
         String LoadData;
         @Override
         protected void onPreExecute() {
@@ -64,43 +65,10 @@ public class ServertestActivity extends AppCompatActivity {
         }
 
         @Override
-        protected Void doInBackground(Void... param) {
+        protected String doInBackground(String... params) {
             // TODO Auto-generated method stub
-//            try {
-//                // httpclient-4.2.2.jar와 httpcore-4.2.2.jar를 mvnrepository에서 찾아 Files에 Download jars를 해 받음
-//                // app/libs에 추가
-//                // 이때, android상태에서는 libs가 안보임으로 project로 변경해서
-//                // lib에 받은 jar파일 2개를 넣어준다.
-//
-//                // http client 객체
-//                HttpClient http = new DefaultHttpClient();
-//                // post 방식으로 전송하는 객체
-//                HttpPost httpPost = new HttpPost("http://52.78.143.125:8080/showme/GetProduct");
-//                // http클라이언트.execute(httppost객체) : 웬서버에 데이터를 전달
-//                // 결과(json)가 response로 넘어옴
-//                HttpResponse response = http.execute(httpPost);
-//                // body에 json 스트링이 넘어옴
-//                String body = EntityUtils.toString(response.getEntity());
-//                // string을 JSONObject로 변환
-//                JSONObject jsonObj = new JSONObject(body);
-//                // json객체.get("변수명")
-//                JSONArray jArray = (JSONArray) jsonObj.get("getData");
-//                Log.d("데이터",jArray.toString());
-//                for (int i = 0; i < jArray.length(); i++) {
-//                    // json배열.getJSONObject(인덱스)
-//                    JSONObject row = jArray.getJSONObject(i);
-//                    Product dto = new Product();
-//                    dto.setId(row.getInt("ID"));
-//                    dto.setName(row.getString("NAME"));
-//
-//                    items.add(dto);
-//                }
-//
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-
-
+            String project = (String)params[0];
+            String uid = (String)params[1];
             try {
                 HttpParams httpParameters = new BasicHttpParams();
                 HttpProtocolParams.setVersion(httpParameters, HttpVersion.HTTP_1_1);
@@ -111,13 +79,32 @@ public class ServertestActivity extends AppCompatActivity {
                 HttpConnectionParams.setSoTimeout(httpParameters, 7000);
                 HttpConnectionParams.setTcpNoDelay(httpParameters, true);
 
-                // 주소
-                String postURL = "http://52.78.143.125:8080/showme/DeleteWishProduct";
+                // 주소 : aws서버
+                String postURL = "http://52.78.143.125:8080/showme/";
 
-                HttpPost post = new HttpPost(postURL);
-                ArrayList<NameValuePair> params = new ArrayList<NameValuePair>();
+                // 로컬서버
+//            String postURL = "http://10.0.2.2:8080/showme/InsertUser";
 
-                UrlEncodedFormEntity ent = new UrlEncodedFormEntity(params, HTTP.UTF_8);
+                HttpPost post = new HttpPost(postURL+project);
+                //서버에 보낼 파라미터
+                ArrayList<NameValuePair> postParameters = new ArrayList<NameValuePair>();
+                //파라미터 추가하기
+                postParameters.add(new BasicNameValuePair("uid", uid));
+//            postParameters.add(new BasicNameValuePair("uid", uid));
+//            postParameters.add(new BasicNameValuePair("infoName", infoName));
+//            postParameters.add(new BasicNameValuePair("infoValue", infoValue));
+//            for(int i=0;i<params.length;i++){
+//                postParameters.add(new BasicNameValuePair("\""+(String)params[i]+"\"",(String)params[i]));
+//                System.out.println("\""+(String)params[i]+"\","+(String)params[i]);
+//            }
+//            postParameters.add(new BasicNameValuePair("uid", "0"));
+//            postParameters.add(new BasicNameValuePair("wishProductName", "1"));
+//            postParameters.add(new BasicNameValuePair("value", "123"));
+
+
+
+                //파라미터 보내기
+                UrlEncodedFormEntity ent = new UrlEncodedFormEntity(postParameters, HTTP.UTF_8);
                 post.setEntity(ent);
 
                 long startTime = System.currentTimeMillis();
@@ -127,27 +114,34 @@ public class ServertestActivity extends AppCompatActivity {
                 long elapsedTime = System.currentTimeMillis() - startTime;
                 Log.v("debugging", elapsedTime + " ");
 
+
                 HttpEntity resEntity = responsePOST.getEntity();
-//                if (resEntity != null) {
-//                    LoadData = EntityUtils.toString(resEntity, HTTP.UTF_8);
-//                    Log.i("가져온 데이터",LoadData);
-//                    JSONObject jsonObj =new JSONObject(LoadData);
-//                    // json객체.get("변수명")
-//                    JSONArray jArray = (JSONArray)jsonObj.get("getData");
-//                    items=new ArrayList<Product>();
-//                    for(int i=0; i<jArray.length();i++){
-//                        // json배열.getJSONObject(인덱스)
-//                        JSONObject row = jArray.getJSONObject(i);
-//                        Product dto =new Product();
-//                        dto.setId(row.getInt("ID"));
-//                        dto.setName(row.getString("NAME"));
-//
-//                        // ArrayList에 add
-//                        items.add(dto);
-//                        Log.i("가져온 데이터",dto.getId()+"");
-//                        Log.i("가져온 데이터",dto.getName()+"");
-//                    }
-//                }
+                if (resEntity != null) {
+                    LoadData = EntityUtils.toString(resEntity, HTTP.UTF_8);
+                    Log.i("가져온 데이터", LoadData);
+                    JSONObject jsonObj = new JSONObject(LoadData);
+                    // json객체.get("변수명")
+                    JSONArray jArray = (JSONArray) jsonObj.get("getData");
+                    items = new ArrayList<Product>();
+                    for (int i = 0; i < jArray.length(); i++) {
+                        // json배열.getJSONObject(인덱스)
+                        JSONObject row = jArray.getJSONObject(i);
+                        Product dto = new Product();
+                        dto.setId(row.getInt("ID"));
+                        dto.setName(row.getString("NAME"));
+
+                        // ArrayList에 add
+                        items.add(dto);
+                        Log.i("가져온 데이터", dto.getId() + "");
+                        Log.i("가져온 데이터", dto.getName() + "");
+                    }
+                }
+                if(responsePOST.getStatusLine().getStatusCode()==200){
+                    System.out.println("오류없음");
+                }
+                else{
+                    System.out.println("오류");
+                }
 
             } catch (Exception e) {
                 e.printStackTrace();
@@ -156,7 +150,7 @@ public class ServertestActivity extends AppCompatActivity {
         }
 
         @Override
-        protected void onPostExecute(Void aVoid) {
+        protected void onPostExecute(String result) {
             pDialog.dismiss();
 
         }
