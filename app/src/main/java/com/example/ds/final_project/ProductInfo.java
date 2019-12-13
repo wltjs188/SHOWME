9864dabdc08e8065d27083d846b374ac2875c628
chaeyoon <yoonchae125@naver.com>
2019-12-13 22:51:37 +0900

    ㅎㅎ

--- app/src/main/java/com/example/ds/final_project/ProductInfo.java
+++ app/src/main/java/com/example/ds/final_project/ProductInfo.java
@@ -27,11 +27,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.bumptech.glide.Glide;
-<<<<<<< HEAD
 import com.example.ds.final_project.db.DTO.WishProduct;
-=======
-import com.example.ds.final_project.db.DAO.InsertWishProduct;
->>>>>>> 97e923df3e5a39a14e60769403599c2d4d23bf66
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.kakao.kakaolink.v2.KakaoLinkResponse;
@@ -701,98 +697,12 @@ public class ProductInfo extends AppCompatActivity {
             WishBtnChanged(infoBool);
         }
     }
-<<<<<<< HEAD
     private void insertFail(){
         infoBool=false;
         WishBtnChanged(infoBool);
         Toast.makeText(getApplicationContext(),"별칭이 중복되어 관심상품 등록에 실패했습니다.",Toast.LENGTH_LONG).show();
     }
     private void showResult(String mJsonString){
-=======
-
-    private class CheckWishProduct {
-
-    }
-
-//    public class InsertWishProduct extends AsyncTask<String, Void, String> {
-//        //    ProgressDialog progressDialog;
-//        String TAG = "phptest";
-//        @Override
-//        protected void onPreExecute() {
-//            super.onPreExecute();
-//        }
-//        @Override
-//        protected void onPostExecute(String result) {
-//            super.onPostExecute(result);
-//
-//            if (result == null||result.equals("")){
-//                Toast.makeText(ProductInfo.this, "관심 상품으로 등록되었습니다.", Toast.LENGTH_SHORT).show();
-//            }
-//            else {
-//                mJsonString = result;
-//                showResult2();
-//            }
-//            Log.d(TAG, "관심상품 등록2" + result);
-//        }
-//        @Override
-//        protected String doInBackground(String... params) {
-//
-//            String uid = (String)params[1];
-//            String productId = (String)params[2];
-//            String optionNum = (String)params[3];
-//            String image = (String)params[4];
-//            String info = (String)params[5];
-//            String wishProductName = (String)params[6];
-//            String serverURL = (String)params[0];
-//            String postParameters = "uid=" + uid + "&productId=" + productId+"&optionNum=" + optionNum+"&image=" + image+"&info=" + info+"&wishProductName=" + wishProductName ;
-//
-//            try {
-//
-//                URL url = new URL(serverURL);
-//                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-//
-//                httpURLConnection.setReadTimeout(5000);
-//                httpURLConnection.setConnectTimeout(5000);
-//                httpURLConnection.setRequestMethod("POST");
-//                httpURLConnection.connect();
-//
-//                OutputStream outputStream = httpURLConnection.getOutputStream();
-//                outputStream.write(postParameters.getBytes("UTF-8"));
-//                outputStream.flush();
-//                outputStream.close();
-//
-//                int responseStatusCode = httpURLConnection.getResponseCode();
-//                Log.d(TAG, "POST response code - " + responseStatusCode);
-//
-//                InputStream inputStream;
-//                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
-//                    inputStream = httpURLConnection.getInputStream();
-//                }
-//                else{
-//                    inputStream = httpURLConnection.getErrorStream();
-//                }
-//
-//                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
-//                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-//                StringBuilder sb = new StringBuilder();
-//                String line = null;
-//                while((line = bufferedReader.readLine()) != null){
-//                    sb.append(line);
-//                }
-//                bufferedReader.close();
-//
-//                return sb.toString().trim();
-//            } catch (Exception e) {
-//                error=1;
-//                Log.d(TAG, "UpdateData: Error ", e);
-//                Log.d("에러",e.getMessage());
-//                return "error";
-//            }
-//
-//        }
-//    }
-    private void showResult(){
->>>>>>> 97e923df3e5a39a14e60769403599c2d4d23bf66
         // int count=0;
         String TAG_JSON="getData";
         try {
