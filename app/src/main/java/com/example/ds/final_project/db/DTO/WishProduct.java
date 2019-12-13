b08b7ca80547d59507681ac9253a0ed6429fde6a
chaeyoon <yoonchae125@naver.com>
2019-12-13 22:46:32 +0900

    별칭 중복 토스트

--- app/src/main/java/com/example/ds/final_project/db/DTO/WishProduct.java
+++ app/src/main/java/com/example/ds/final_project/db/DTO/WishProduct.java
@@ -1,8 +1,36 @@
 package com.example.ds.final_project.db.DTO;
 
-public class WishProduct extends Product {
+public class WishProduct {
     String uid;
     String alias;
+    String id;
+    String image;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    String info;
 
     public String getUid() {
         return uid;
