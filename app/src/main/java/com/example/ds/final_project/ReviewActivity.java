cd3cc413eed6bc1b97badeedf746d29f61e9118d
Haeeul <etoilune00@gmail.com>
2019-12-13 17:41:29 +0900

    리뷰 크롤링 연결

--- app/src/main/java/com/example/ds/final_project/ReviewActivity.java
+++ app/src/main/java/com/example/ds/final_project/ReviewActivity.java
@@ -32,12 +32,13 @@ public class ReviewActivity extends AppCompatActivity {
     private String productUrl = "";
     private String txtAllRating ="";
     private String txtKeyword ="";
-    String maxstit = "";
-    int maxscore = 0;
+    String stit = "";
+    String score = "";
+    int num = 0;
 
 
     //11번가 기본 주소 - 상품id에 붙여서 파싱페이지로 만들것임
-    private String testUrl = "http://www.11st.co.kr/product/SellerProductDetail.tmall?method=getSellerProductDetail&prdNo=";
+    private String testUrl = "https://store.musinsa.com/app/product/detail/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,13 +69,13 @@ public class ReviewActivity extends AppCompatActivity {
         //keyword.setText(txtKeyword); //키워드 넣으면됨
 
         reviewDataList = new ArrayList<ReviewData>();
-        reviewDataList.add(new ReviewData(1,"4.5","신축성이 너무 좋아요")); //순서,평점,리뷰 순으로 넣으면됨
-        reviewDataList.add(new ReviewData(2,"3.0","핏이 예뻐요"));
-        reviewDataList.add(new ReviewData(3,"4.2","색상이 맘에 들어요."));
-        reviewDataList.add(new ReviewData(4,"4.2","색상이 맘에 들어요."));
-        reviewDataList.add(new ReviewData(5,"4.2","색상이 맘에 들어요."));
-        reviewDataList.add(new ReviewData(6,"4.2","색상이 맘에 들어요."));
-        reviewDataList.add(new ReviewData(6,"4.2","색상이 맘에 들어요."));
+//        reviewDataList.add(new ReviewData(1,"4.5","신축성이 너무 좋아요")); //순서,평점,리뷰 순으로 넣으면됨
+//        reviewDataList.add(new ReviewData(2,"3.0","핏이 예뻐요"));
+//        reviewDataList.add(new ReviewData(3,"4.2","색상이 맘에 들어요."));
+//        reviewDataList.add(new ReviewData(4,"4.2","색상이 맘에 들어요."));
+//        reviewDataList.add(new ReviewData(5,"4.2","색상이 맘에 들어요."));
+//        reviewDataList.add(new ReviewData(6,"4.2","색상이 맘에 들어요."));
+//        reviewDataList.add(new ReviewData(6,"4.2","색상이 맘에 들어요."));
 
     }
     @Override
@@ -115,35 +116,52 @@ public class ReviewActivity extends AppCompatActivity {
             try {
                 //파싱페이지 넣어주기
                 Document doc = Jsoup.connect(productUrl).get();
-                // 리뷰부분 접근
-                Elements contents = doc.select("div.deal_rank_ct");
+                System.out.println("주소"+productUrl);
+                // 키워드부분 접근
+                Elements contents = doc.select("div.wrap-estimate-avg");
                 System.out.println("전체 : " + contents.text() + "\n");
 
                 for (Element e : contents) {
                     //평점 출력
-                    String rank = e.getElementsByTag("strong").text();
+                    String rank = e.select("span.rate").text();
                     txtAllRating += "평점 : " + rank + "\n";
-                    System.out.println("평점"+txtAllRating);
+                    //System.out.println("평점"+txtAllRating);
 
                     // 평가항목 접근
-                    Elements cts = e.select("div.ct");
+                    Elements cts = e.select("div.lv-contents");
+
                     for(Element j : cts){
                         //평가항목 출력
-                        String ct = j.getElementsByTag("h5").text();
+                        String ct = j.select("div.tit").text();
 
                         // 세부항목 접근
-                        Elements stits = j.select("li");
+                        Elements stits = j.select("li.on");
                         for(Element i : stits){
                             //세부항목 출력
-                            String stit = i.select("span.stit").text();
-                            String score = i.select("span.num").text();
-                            if(Integer.parseInt(score) > maxscore){
-                                maxscore = Integer.parseInt(score);
-                                maxstit = stit;
-                            }
-                        }txtKeyword += ct + ":" + maxstit + "("+ maxscore + ") \n";
+                            stit = i.select("div.label").text();
+                            //System.out.println("세부항목"+stit);
+                            score = i.select("div.per").text();
+                        }txtKeyword += ct + ":" + stit + "("+ score + ") \n";
 
-                    }System.out.println("평가"+txtKeyword);
+                    }//System.out.println("평가"+txtKeyword);
+                }
+
+                // 리뷰부분 접근
+                Elements reviewbox = doc.select("div#style_estimate_list");
+                Elements reviewContent = reviewbox.select("div.nslist_post");
+
+                for (Element r : reviewContent) {
+                    num++;
+                    //리뷰 제목
+                    String rt = r.select("div.tit").text();
+                    //리뷰 내용
+                    String rank = r.select("span.content-review").text();
+
+                    ReviewData reviewD = new ReviewData(num,rt,rank);
+                    //System.out.println("번호:"+reviewD.getNum()+"제목:"+reviewD.getTitle()+"내용"+reviewD.getReview());
+
+                    //add가 안됨..!!
+                    reviewDataList.add(reviewD);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
@@ -197,7 +215,7 @@ class ReviewAdapter extends BaseAdapter {
         TextView review = (TextView)view.findViewById(R.id.review);
 
         num.setText(reviewList.get(position).getNum()+"번");
-        rating.setText("평점 : "+reviewList.get(position).getRating());
+        rating.setText("평점 : "+reviewList.get(position).getTitle());
         review.setText(reviewList.get(position).getReview());
         return view;
     }
