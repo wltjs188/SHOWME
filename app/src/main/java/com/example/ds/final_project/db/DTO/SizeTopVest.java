a5473cc8fb555ce1650e989cb5b0096e04cf8288
chaeyoon <yoonchae125@naver.com>
2019-12-21 19:35:33 +0900

    vest

---
+++ app/src/main/java/com/example/ds/final_project/db/DTO/SizeTopVest.java
@@ -0,0 +1,31 @@
+package com.example.ds.final_project.db.DTO;
+
+public class SizeTopVest extends Size {
+    String total;
+    String sholder;
+    String breast;
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    public String getSholder() {
+        return sholder;
+    }
+
+    public void setSholder(String sholder) {
+        this.sholder = sholder;
+    }
+
+    public String getBreast() {
+        return breast;
+    }
+
+    public void setBreast(String breast) {
+        this.breast = breast;
+    }
+}
