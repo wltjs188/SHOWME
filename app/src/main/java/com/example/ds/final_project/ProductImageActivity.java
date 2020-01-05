93592d60edb61fb20173ba6a580dab69f2ffd36f
chaeyoon <yoonchae125@naver.com>
2020-01-05 17:50:36 +0900

    수정

--- app/src/main/java/com/example/ds/final_project/ProductImageActivity.java
+++ app/src/main/java/com/example/ds/final_project/ProductImageActivity.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -21,7 +22,8 @@ public class ProductImageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_product_image);
 
         getSupportActionBar().setTitle("상품 이미지");
-
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);//뒤로가기 버튼
+        
         btn = findViewById(R.id.btn_close);
         product_image = findViewById(R.id.product_image);
 
@@ -37,6 +39,11 @@ public class ProductImageActivity extends AppCompatActivity {
             }
         });
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
     public boolean onOptionsItemSelected(MenuItem item) { //뒤로가기 버튼 실행
         Intent homeIntent=new Intent(this,ChatbotActivity.class);
